f1c0a5ae6a0303e3bc6fa6bdcda5f053979c3601
V4mpw0L <125161567+V4mpw0L@users.noreply.github.com>
2025-10-20 19:15:43 -0700

    att

--- js/ui/InventoryUI.js
+++ js/ui/InventoryUI.js
@@ -291,6 +291,7 @@ export default class InventoryUI {
         align-items: center;
         gap: 0.125rem;
         text-shadow: 0 1px 2px rgba(0, 0, 0, 0.5);
+        margin-bottom: 0.25rem;
       }
 
       .inventory-slot-count {
